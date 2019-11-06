2dca06d609f433467bb5d805d3a213d769a9fb82
tuyetrinhvo <tuyetrinhvo@gmail.com>
2019-11-06 15:56:56 +0100

    create get and post

---
+++ src/main/java/com/cesi/ApplicationAccessingDatabase.java
@@ -0,0 +1,11 @@
+package com.cesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationAccessingDatabase {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationAccessingDatabase.class, args);
+    }
+}
---
+++ src/main/java/com/cesi/UserRepository.java
@@ -0,0 +1,8 @@
+package com.cesi;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
